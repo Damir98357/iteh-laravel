070b624a7e27e1a45bc0373850892b51589c7332
Damir98357 <jayzzy8@gmail.com>
2023-05-18 18:56:25 +0200

    user-registration-and-login

--- webshop/routes/api.php
+++ webshop/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\CustomerController;
 
 /*
@@ -33,3 +34,6 @@ Route::get('/orders/{id}', [OrderController::class, 'show']);
 
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
